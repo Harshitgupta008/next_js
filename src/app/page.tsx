44725002f73c8e94a4e606525aa8e697a7d16687
harshitgupta008 <harshitguptas0408@gmail.com>
2025-01-19 13:01:33 +0530

    add next js e-comerse using next js and redux rtk querry

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Home from "@/component/Home/Home";
+
+
+const HomePage: React.FC = () => {
+
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default HomePage;
